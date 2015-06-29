0912814170b3d63a72b4cda6e745e97ccfe744be
Douglas Castro da Silva <douglascastrorj@gmail.com>
2015-06-29 16:32:21 -0300

    loop do jogo

--- main.cpp
+++ main.cpp
@@ -1,3 +1,5 @@
+#define FPS 30
+
 #include <iostream>
 
 #include "SDL/SDL.h"
@@ -16,20 +18,52 @@ int main(){
 
 	//load images and set ap screen
 	screen = SDL_SetVideoMode(900,900,8,SDL_SWSURFACE);
-	sprite = IMG_Load("image/block.png");
-
-
+	sprite = IMG_Load("image/sprite.png");
 
 	if(sprite == NULL)
 		cout << "ghjklç\n";
 
-	
+
 	Mapa *m = new Mapa(sprite);
 
-	SDL_BlitSurface(sprite,NULL,screen,NULL);
-	
-	// m->blit(screen);
-	SDL_Delay(1000);
+	SDL_Event event;
+	bool running = true;	
+	while(running){
+
+		int t1 = SDL_GetTicks();
+
+		m->blit(screen);
+
+		//handling the events
+		while(SDL_PollEvent(&event)){
+			if( event.type==SDL_QUIT){
+				running = false;
+			}
+			if( event.type == SDL_MOUSEMOTION )	{
+				
+			}
+			if(event.type == SDL_KEYDOWN){
+				switch( event.key.keysym.sym )
+				{
+					case SDLK_UP: {  break;}
+					case SDLK_DOWN: {  break;}
+					case SDLK_LEFT:{  break;}
+					case SDLK_RIGHT:{ break;}
+				}
+			}
+		}
+
+		int t2 = SDL_GetTicks();
+        int wait = t2 - t1;
+        wait = (1000/FPS) - wait;
+
+        if(wait > 0)
+			SDL_Delay(wait);
+		
+
+		SDL_Flip(screen);
+
+	}
 	SDL_Quit();
 	return 0;
 }
